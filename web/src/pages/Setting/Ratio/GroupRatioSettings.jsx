215aab19a648ce8d3c79d488fd3781694dd7f78a
hujiajun <hjj114944713@163.com>
2026-01-19 11:05:56 +0800

    文案展示内容修改

--- web/src/pages/Setting/Ratio/GroupRatioSettings.jsx
+++ web/src/pages/Setting/Ratio/GroupRatioSettings.jsx
@@ -119,10 +119,10 @@ export default function GroupRatioSettings(props) {
         <Row gutter={16}>
           <Col xs={24} sm={16}>
             <Form.TextArea
-              label={t('分组倍率')}
+              label={t('添加用户名称')}
               placeholder={t('为一个 JSON 文本，键为分组名称，值为倍率')}
               extraText={t(
-                '分组倍率设置，可以在此处新增分组或修改现有分组的倍率，格式为 JSON 字符串，例如：{"vip": 0.5, "test": 1}，表示 vip 分组的倍率为 0.5，test 分组的倍率为 1',
+                '添加用户名称设置，可以在此处新增分组或修改现有分组的倍率，格式为 JSON 字符串，例如：{"vip": 0.5, "test": 1}，表示 vip 分组的倍率为 0.5，test 分组的倍率为 1',
               )}
               field={'GroupRatio'}
               autosize={{ minRows: 6, maxRows: 12 }}
@@ -141,7 +141,7 @@ export default function GroupRatioSettings(props) {
         <Row gutter={16}>
           <Col xs={24} sm={16}>
             <Form.TextArea
-              label={t('用户可选分组')}
+              label={t('令牌分组')}
               placeholder={t('为一个 JSON 文本，键为分组名称，值为分组描述')}
               extraText={t(
                 '用户新建令牌时可选的分组，格式为 JSON 字符串，例如：{"vip": "VIP 用户", "test": "测试"}，表示用户可以选择 vip 分组和 test 分组',
@@ -165,7 +165,7 @@ export default function GroupRatioSettings(props) {
         <Row gutter={16}>
           <Col xs={24} sm={16}>
             <Form.TextArea
-              label={t('分组特殊倍率')}
+              label={t('特殊模型分组折扣')}
               placeholder={t('为一个 JSON 文本')}
               extraText={t(
                 '键为分组名称，值为另一个 JSON 对象，键为分组名称，值为该分组的用户的特殊分组倍率，例如：{"vip": {"default": 0.5, "test": 1}}，表示 vip 分组的用户在使用default分组的令牌时倍率为0.5，使用test分组时倍率为1',
@@ -189,7 +189,7 @@ export default function GroupRatioSettings(props) {
         <Row gutter={16}>
           <Col xs={24} sm={16}>
             <Form.TextArea
-              label={t('分组特殊可用分组')}
+              label={t('用户令牌分组权限设置')}
               placeholder={t('为一个 JSON 文本')}
               extraText={t(
                 '键为用户分组名称，值为操作映射对象。内层键以"+:"开头表示添加指定分组（键值为分组名称，值为描述），以"-:"开头表示移除指定分组（键值为分组名称），不带前缀的键直接添加该分组。例如：{"vip": {"+:premium": "高级分组", "special": "特殊分组", "-:default": "默认分组"}}，表示 vip 分组的用户可以使用 premium 和 special 分组，同时移除 default 分组的访问权限',

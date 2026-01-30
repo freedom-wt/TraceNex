dab4efd1fedcb24c5e3cc6498ed8c035f3127232
hujiajun <hjj1149244713@163.com>
2026-01-30 14:52:26 +0800

    新增文档地址和文件 && 放开对注册的限制 && 登录顺序的修改

--- web/src/pages/Setting/Ratio/GroupRatioSettings.jsx
+++ web/src/pages/Setting/Ratio/GroupRatioSettings.jsx
@@ -119,10 +119,10 @@ export default function GroupRatioSettings(props) {
         <Row gutter={16}>
           <Col xs={24} sm={16}>
             <Form.TextArea
-              label={t('分组倍率')}
-              placeholder={t('为一个 JSON 文本，键为分组名称，值为倍率')}
+              label={t('分组折扣')}
+              placeholder={t('为一个 JSON 文本，键为分组名称，值为折扣')}
               extraText={t(
-                '分组倍率设置，可以在此处新增分组或修改现有分组的倍率，格式为 JSON 字符串，例如：{"vip": 0.5, "test": 1}，表示 vip 分组的倍率为 0.5，test 分组的倍率为 1',
+                '分组折扣设置，可以在此处新增分组或修改现有分组的折扣，格式为 JSON 字符串，例如：{"黄金": 0.9, "铂金": 0.89}，表示分组黄金的倍率为 0.8，铂金 分组的倍率为 0.85',
               )}
               field={'GroupRatio'}
               autosize={{ minRows: 6, maxRows: 12 }}
@@ -141,10 +141,10 @@ export default function GroupRatioSettings(props) {
         <Row gutter={16}>
           <Col xs={24} sm={16}>
             <Form.TextArea
-              label={t('用户可选分组')}
+              label={t('令牌分组')}
               placeholder={t('为一个 JSON 文本，键为分组名称，值为分组描述')}
               extraText={t(
-                '用户新建令牌时可选的分组，格式为 JSON 字符串，例如：{"vip": "VIP用户", "test": "测试"}，表示用户可以选择vip分组和test分组',
+                '用户新建令牌时可选的分组，格式为 JSON 字符串，例如：{"黄金": "黄金用户", "铂金": "铂金用户"}，表示用户可以选择黄金用户分组和铂金用户分组',
               )}
               field={'UserUsableGroups'}
               autosize={{ minRows: 6, maxRows: 12 }}
@@ -165,10 +165,10 @@ export default function GroupRatioSettings(props) {
         <Row gutter={16}>
           <Col xs={24} sm={16}>
             <Form.TextArea
-              label={t('分组特殊倍率')}
+              label={t('特殊模型分组折扣')}
               placeholder={t('为一个 JSON 文本')}
               extraText={t(
-                '键为分组名称，值为另一个 JSON 对象，键为分组名称，值为该分组的用户的特殊分组倍率，例如：{"vip": {"default": 0. 5, "test": 1}}，表示vip分组的用户在使用default分组的令牌时倍率为0.5，使用test分组时倍率为1',
+                '键为用户分组，值为该用户的特殊分组折扣，例如：{"黄金": {"豆包模型分组": 0. 8}}，表示黄金用户在使用豆包模型分组的令牌时折扣为0.8',
               )}
               field={'GroupGroupRatio'}
               autosize={{ minRows: 6, maxRows: 12 }}
@@ -189,10 +189,10 @@ export default function GroupRatioSettings(props) {
         <Row gutter={16}>
           <Col xs={24} sm={16}>
             <Form.TextArea
-              label={t('分组特殊可用分组')}
+              label={t('用户令牌创建权限')}
               placeholder={t('为一个 JSON 文本')}
               extraText={t(
-                '键为用户分组名称，值为操作映射对象。内层键以"+:"开头表示添加指定分组（键值为分组名称，值为描述），以"-:"开头表示移除指定分组（键值为分组名称），不带前缀的键直接添加该分组。例如：{"vip": {"+:premium": "高级分组", "special":"特殊分组", "-:default": "默认分组"}}，表示vip分组的用户可以使用premium和special分组，同时移除 default 分组的访问权限',
+                '键为用户分组，值为操作映射对象。内层键以"+:"开头表示添加指定分组（键值为分组名称，值为描述），以"-:"开头表示移除指定分组（键值为分组名称），不带前缀的键直接添加该分组。例如：{"黄金": {"+:豆包模型分组": "特殊模型分组", "-":"default":"默认分组"}}，表示黄金用户可以使用豆包模型分组，同时移除 default 分组的访问权限',
               )}
               field={'group_ratio_setting.group_special_usable_group'}
               autosize={{ minRows: 6, maxRows: 12 }}
---
+++ web/src/pages/docs/TraceNexDoc.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function TraceNexDoc() {
+    const url ='https://www.yuque.com/wangteng-w8rzb/xxa8h4/xgm84a15566wmbu4?singleDoc# 《TraceNex》';
+
+    return (
+        <div style={{width:'100%',minHeight:'100vh'}}>
+            <iframe
+            src={url}
+            width='100%'
+            height='100vh'
+            title="文档"
+            style={{border:'none', height:'100vh'}}
+            />
+            
+        </div>
+    )
+}
